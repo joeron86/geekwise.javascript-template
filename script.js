cbd38c1043fdb5f6c7105b604c7650a279b1adbf
Joseph Halderman <josephhalderman@Josephs-MacBook-Pro-2.local>
2016-04-18 18:17:47 -0700

    Fishished Practice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello from the JavaScript file");
+
+document.getElementById('header').style.color = "#00adef";
+document.getElementById('header').style.fontSize = "80px";
+document.getElementById('header').style.backgroundColor = "#f8ed31"
+
+function appendDiv(arg1) {
+  var divElement = document.createElement("div");
+  divElement.innerHTML = arg1;
+  document.body.appendChild(divElement);
+}
+
+function appendItems(arg1) {
+  for (var i = 0; i < someArray.length; i++) {
+    appendDiv(someArray[i]);
+  }
+}
+var someArray = [1,2,3,4];
+
+appendItems(someArray);
